3bf40f1a280cfee218474391406c1e2d41a604a3
vasudevchavan <30864214+vasudevchavan@users.noreply.github.com>
2018-09-12 18:44:37 +0530

    Hello World program

---
+++ Hello World.py
@@ -0,0 +1,8 @@
+Greeting =  'Welcome to Python Programming'
+print(Greeting)
+
+
+ # Python Uses the Source code present within the file created Hello World.py
+ # Creates a byte code or compiled code Hello World.pyc which will be the input for Python Virtual machine
+ # Now the byte code is processed by PVM and gives the required output.
+
