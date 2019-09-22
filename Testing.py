b43296d0026b6a860ada2c9323076dba362534c7
vasudevchavan <30864214+vasudevchavan@users.noreply.github.com>
2019-09-22 22:10:54 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,3 +1,3 @@
 Print("we are in checking a new file")
 Print("we are moving ahead with code")
-Print("we are moving ahead with code and testing inside Centos")
+Print("we are moving ahead with code and testing inside Centos fixed the issue")
